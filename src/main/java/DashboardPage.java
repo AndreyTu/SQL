f66aa6da821f27c2119ed2e6376cea95497bc240
AndreyTu <139591226+AndreyTu@users.noreply.github.com>
2024-08-05 22:46:08 +0300

    second commit

--- src/main/java/DashboardPage.java
+++ src/main/java/DashboardPage.java
@@ -1,6 +1,5 @@
-package ru.netology.banklogin.page;
-
 import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
